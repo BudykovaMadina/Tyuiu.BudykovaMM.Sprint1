7ca67ec8f92147499508c6f68641b09515987c28
Мадина <mbudykova@mail.ru>
2025-09-28 19:34:32 +0500

    Task4

---
+++ Tyuiu.BudykovaMM.Sprint1.Task4.V4.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BudykovaMM.Sprint1.Task4.V4.Lib;
+namespace Tyuiu.BudykovaMM.Sprint1.Task4.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 5;
+            double wait = 2;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BudykovaMM.Sprint1.Task4.V4/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.BudykovaMM.Sprint1.Task4.V4.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнила: Будыкова М. М. | ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #4                                                              *");
+        Console.WriteLine("* Выполнила: Будыкова Мадина Маратовна | ПИНб-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* вычисляет результат по формуле (1+x*y)/|x+2| и печатает его на экране.  *");
+        Console.WriteLine("* Ответ округлите до 3 знаков после запятой.                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        
+        int x;
+        int y;
+
+        Console.WriteLine("Введите значение X:");
+        x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите значение Y:");
+        y = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine(" (1+x*y)/|x+2| = " + ds.Calculate(x,y));
+
+        Console.ReadKey();
+    }
+}
