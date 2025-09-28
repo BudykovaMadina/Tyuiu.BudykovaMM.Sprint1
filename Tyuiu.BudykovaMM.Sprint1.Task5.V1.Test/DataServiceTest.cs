c908d2040eecc97a83f3af37170f898dd4cb135a
Мадина <mbudykova@mail.ru>
2025-09-28 20:38:15 +0500

    Task5

---
+++ Tyuiu.BudykovaMM.Sprint1.Task5.V1.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.BudykovaMM.Sprint1.Task5.V1.Lib;
+namespace Tyuiu.BudykovaMM.Sprint1.Task5.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            double x1 = 1;
+            double y1 = 1;
+            double x2 = 3;
+            double y2 = 1;
+            DataService ds = new DataService();
+            double res = ds.DistanceBetweenDots(x1, y1, x2, y2);
+            int result = Convert.ToInt32(res);
+            int wait = 2;
+            Assert.AreEqual(wait, result);
+
+
+        }
+    }
+}
---
+++ Tyuiu.BudykovaMM.Sprint1.Task5.V1/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.BudykovaMM.Sprint1.Task5.V1.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнила: Будыкова М. М. | ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #1                                                              *");
+        Console.WriteLine("* Выполнила: Будыкова Мадина Маратовна | ПИНб-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая решает следующую задачу: Найти расстояние   *");
+        Console.WriteLine("* между двумя точками с заданными координатами(x, y).                     *");
+        Console.WriteLine("* Ответ привести к целому с помощью класса Convert.                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+         
+        Console.WriteLine("Введите значение X1:");
+        double x1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите значение Y1:");
+        double y1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите значение X2:");
+        double x2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите значение Y2:");
+        double y2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        int res = Convert.ToInt32(ds.DistanceBetweenDots(x1, y1, x2, y2));
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
