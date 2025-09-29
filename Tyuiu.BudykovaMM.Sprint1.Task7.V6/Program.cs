7b40e48c5e8a17c2ea57a65b784a7e64fa456226
Мадина <mbudykova@mail.ru>
2025-09-29 17:46:37 +0500

    Task7

---
+++ Tyuiu.BudykovaMM.Sprint1.Task7.V6/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.BudykovaMM.Sprint1.Task7.V6.Lib;
+// Написать программу, которая вычисляет математическое выражение
+// по исходным значениям данных, вводимых пользователем.
+// Ответ округлите до 3 знаков после запятой.
+// z = ( 1 + 1 / x^2 )^x - 12 * x^2 *y
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #1 | Выполнила: Будыкова М. М. | ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #2                                                              *");
+        Console.WriteLine("* Выполнила: Будыкова Мадина Маратовна | ПИНб-25-1                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет математическое выражение          *");
+        Console.WriteLine("* по исходным значениям данных, вводимых пользователем.                   *");
+        Console.WriteLine("* Ответ округлите до 3 знаков после запятой.                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* z = ( 1 + 1 / x^2 )^x - 12 * x^2 *y                                     *");
+
+        double x, y;
+        Console.WriteLine("Введите значение Х:");
+        x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите значение Y:");
+        y = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("z = " + ds.Calculate(x,y));
+
+        Console.ReadKey();
+    }
+}
